9eafb293154d19cb993cd10a5cd7542fb4e0c72a
Raphael Lima <raphaeldesousalm@gmal.com>
2023-09-19 22:45:36 -0300

    feat: add setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div className="container-max">
+      <Header/>
+      <main>
+        teste
+      </main>
+    </div>
+  )
+}
+export default App
